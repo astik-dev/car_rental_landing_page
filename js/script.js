befc3f098fae8074c3d363e76008a42dd0a954be
ASTIK <astik.ytt@gmail.com>
2023-01-21 14:47:44 +0100

    Add responsiveness to main__description

--- js/script.js
+++ js/script.js
@@ -52,3 +52,39 @@ window.addEventListener("resize", function (event) {
 		menu.style.height = window.innerHeight + window.pageYOffset + "px";
 	}
 });
+
+
+
+// Lazy loading
+const lazyImages = document.querySelectorAll(".img-lazy-loading[data-src]");
+const windowHeight = document.documentElement.clientHeight;
+let lazyImagesPositions = [];
+
+if (lazyImages.length > 0) {
+	lazyImages.forEach(img => {
+		if (img.dataset.src) {
+			lazyImagesPositions.push(img.getBoundingClientRect().top + pageYOffset);
+			lazyScrollCheck();
+		}
+	});
+}
+
+function lazyScrollCheck() {
+	let imgIndex = lazyImagesPositions.findIndex(
+		item => pageYOffset > item - windowHeight
+	);
+	if (imgIndex >= 0) {
+		if(lazyImages[imgIndex].dataset.src) {
+			lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
+			lazyImages[imgIndex].removeAttribute("data-src");
+		}
+		delete lazyImagesPositions[imgIndex];
+	}
+}
+function lazyScroll() {
+	if (document.querySelectorAll(".img-lazy-loading[data-src]").length > 0) {
+		lazyScrollCheck();
+	}
+}
+
+window.addEventListener("scroll", lazyScroll);
